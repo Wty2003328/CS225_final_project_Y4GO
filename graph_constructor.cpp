0431bdc193c0b31ad9c1bb03da446ba6183d2deb
Wty2003328 <116847915+Wty2003328@users.noreply.github.com>
2022-11-29 11:45:39 -0600

    bfs search for least transfer done

--- graph_constructor.cpp
+++ graph_constructor.cpp
@@ -10,12 +10,56 @@ container::container(std::vector<Vertex>&vertices, std::vector<std::pair<Vertex,
 		inner.insertEdge(it->first,it->second);
 		inner.setEdgeWeight(it->first,it->second,distances[it-edges.begin()]);
 	}
-	adjcentmatrix.resize(vertices.size(),std::vector<Vertex>());
+	
+}
+container::container(std::vector<Vertex>&vertices, std::vector<std::pair<Vertex,Vertex>>&edges):inner(Graph(false,true))
+{
 	for(auto it=vertices.begin();it!=vertices.end();++it)
 	{
-		adjcentmatrix[it-vertices.begin()]=inner.getAdjacent(*it);
+		inner.insertVertex(*it);
+	}
+	for(auto it=edges.begin();it!=edges.end();++it)
+	{
+		inner.insertEdge(it->first,it->second);
 	}
 	
 }
-
+std::vector<Vertex> container::solvebyBFS(std::vector<Vertex> &airports, Vertex start, Vertex end)
+{
+    std::unordered_map<Vertex,Vertex> visited;
+    std::vector<Vertex> solution;
+    for(auto it=airports.begin();it!=airports.end();++it)
+    {
+        visited[*it]="NONE";
+    }
+    std::queue<Vertex> solve;
+    solve.push(start);
+    visited.find(start)->second="start";
+    while (!solve.empty())
+    {
+        Vertex v = solve.front();
+        solve.pop();
+        if(v==end)
+        break;
+        std::vector<Vertex> neighbours=inner.getAdjacent(v);
+        for(auto it=neighbours.begin();it!=neighbours.end();++it)
+        {
+            auto it1=visited.find(*it);
+            if(it1->second=="NONE")
+            {
+                solve.push(*it);
+                it1->second=v;
+            }
+        }
+    }
+    auto it=visited.find(end);
+    solution.insert(solution.begin(),end);
+    while(it->second!="start")
+    {
+        solution.insert(solution.begin(),it->second);
+        it=visited.find(it->second);
+    }
+    solution.insert(solution.begin(),it->second);
+    return solution;
+}
 
--- graph_constructor.h
+++ graph_constructor.h
@@ -5,13 +5,16 @@
 //vector of string: edges
 #include <vector>
 #include <string>
+#include <queue>
+#include <unordered_map>
 #include "edge.h"
 #include "graph.h"
 class container
 { 
 	public:
 		container(std::vector<std::string>&vertices, std::vector<std::pair<std::string,std::string>>&edges, std::vector<double>&distances);
+		container(std::vector<std::string>&vertices, std::vector<std::pair<std::string,std::string>>&edges);
 		Graph inner;
 		container();
-		std::vector<std::vector<std::string>> adjcentmatrix;
+		std::vector<Vertex> solvebyBFS(std::vector<Vertex> &airports, Vertex start, Vertex end);
 };
