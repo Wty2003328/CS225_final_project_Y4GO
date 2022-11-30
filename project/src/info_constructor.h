b23106dc86a472c62cd22c5e2d913c6448d0d773
Wty2003328 <116847915+Wty2003328@users.noreply.github.com>
2022-11-30 16:55:31 -0600

    info_constructor frame

---
+++ project/src/info_constructor.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <vector>
+class info_container
+{
+public:
+	info_container(std::string airports, std::string routes);
+	void clean();
+	void read();
+	std::vector<std::string> generate_vertices();
+	std::vector<std::string> generate_edges();
+	std::vector<double> calculate_dist();
+private:
+
+};
