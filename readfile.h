d8348bfdbdade74bbbb16aec13feaf6cab94b077
SherryChen0922 <yanc7@illinois.edu>
2022-11-30 03:34:28 -0600

    readfile.h 2.0

--- readfile.h
+++ readfile.h
@@ -10,11 +10,13 @@ std::vector<std::vector<string>> readfile(const std::string & filename);
 std::string TrimRight(const std::string & str);
 std::string TrimLeft(const std::string & str);
 void Trim(const std::string & str);
-std::vector<std::vector<string>> temp;
+std::vector<std::vector<string>> temp1;
 struct airport {
     std::string name_;
     int latitude_;
     int longtitude_;
 };
 void devide(string line, vector<string> temp);
-map<std::string, airport> airport;
+map<std::string, airport> airport_;
+std::vector<std::vector<string>> temp2;
+void transfer();
