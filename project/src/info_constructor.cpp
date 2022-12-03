e6cfd64cbca5cfb0c351f638d74b164eede7a30a
yilinzh4399 <yilin19@illinois.edu>
2022-12-03 01:25:52 -0600

    new

--- project/src/info_constructor.cpp
+++ project/src/info_constructor.cpp
@@ -1,4 +1,15 @@
+#include <cmath>
+#include <cstdlib>
+#include <sstream>
+#include <fenv.h>
+#include <signal.h>
+#include <vector>
+#include <stdlib.h>
+#include <unordered_map>
+
 #include "info_constructor.h"
+#include <algorithm>
+
 double info_container::cal_dist(double longitude1, double latitude1, double longitude2, double latitude2)
 {
 	double rad_longtitude1 = pi * longitude1 / 180;
@@ -11,19 +22,113 @@ double info_container::cal_dist(double longitude1, double latitude1, double long
 	dist = dist * EARTH_RAD;
 	return dist;
 }
-info_container::info_container(std::string airports, std::string routes)
+info_container::info_container(std::string & airports, std::string & routes)
 {
-
+	airports_ = airports;
+	routes_ = routes;
 }
 
 void info_container::clean()
 {
-    return;
+    cleanAirport();
+	cleanRoute();
+	return;
+}
+
+void info_container::cleanAirport() {
+    for (int i = 0; i < airport_v.size(); ) {  
+        double latitude = stod(airport_v[i][7]);
+        double longitude = stod(airport_v[i][8]);
+        if (airport_v[i][1].find("Air Base") == std::string::npos) {    //delete airbase
+            if((latitude > -90 && latitude < 90) && (longitude > -180 && longitude < 180)) {    //delete beyond
+				if(airport_v[i].size() == 8) {    //if information lost, then delete it
+					i ++ ;
+				}
+			}       
+        }
+        else {
+            airport_v.erase(airport_v.begin() + i);
+        }
+    }
+}
+
+void info_container::cleanRoute() {   //delete the route that src/dest not in the list
+    for (int i = 0; i < route_v.size(); ) { 
+        if (count(route_v.begin(), route_v.end(), route_v[i][3]) != 0
+            && count(route_v.begin(), route_v.end(), route_v[i][5]) != 0) {
+                i ++ ;
+        }
+        else {
+            route_v.erase(route_v.begin() + i);
+        }
+    }
 }
 
 void info_container::read()
 {
-    return;
+    airport_v = transferFile(airports_);
+	route_v = transferFile(routes_);
+
+	return;
+}
+
+std::string info_container::readfile(const std::string& filename){
+	std::ifstream in(filename);
+
+	std::stringstream strStream;
+	if (in.is_open()) {
+		strStream << in.rdbuf();
+	}
+	return strStream.str();
+}
+
+std::vector<std::vector<std::string>> info_container::transferFile(const std::string & filename){
+    std::string newString = readfile(filename);
+    std::vector<std::string> rows;
+    
+    int num_row = divide(newString, '\n', rows);
+    std::vector<std::vector<std::string>> newV2D;
+
+    for(int i = 0;i < num_row;i ++) {
+        std::vector<std::string> cols;
+        newV2D.push_back({});
+
+        int num_col = divide(rows[i], ',', cols);
+        for(int j = 0;j < num_col;j ++) {
+            std::string single = Trim(cols[j]);
+            newV2D[i].push_back(single);
+        }    
+    }
+    return newV2D;
+}
+
+void info_container::getAirportName() {
+    for(int i = 0; i < airport_v.size();i++) {    
+        AirportName[i] = airport_v[i][1];
+    }
+}
+void info_container::getAirportCode() {
+    for(int i = 0; i < airport_v.size();i++) {    
+        AirportCode[i] = airport_v[i][0];
+    }
+}
+
+void info_container::getRoute() {
+    for(int i = 0; i < route_v.size();i++) {    
+        allRoute[i].first = route_v[i][3];     //first--src, second--dest
+        allRoute[i].second = route_v[i][5];
+    }
+}
+
+void info_container::getStruct() {
+    for(int i = 0; i < airport_v.size();i++) {
+        std::string id = airport_v[i][0];
+        struct airports s;
+        s.code_ = airport_v[i][0];
+        s.name_ = airport_v[i][1];
+        s.latitude_ = airport_v[i][6];
+        s.longtitude_ = airport_v[i][7];
+    }
 }
 
 std::vector<std::string> info_container::generate_vertices()
@@ -39,4 +144,30 @@ std::vector<std::pair<std::string,std::string>> info_container::generate_edges()
 std::vector<double> info_container::calculate_dist()
 {
     return std::vector<double>();
+}
+
+int info_container::divide(const std::string &line, char ch, std::vector<std::string> &temp) {
+    std::string newLine = line;
+    std::string::size_type pos;
+    while((pos = newLine.find(ch)) != std::string::npos) {
+        temp.push_back(newLine.substr(0,pos));
+        newLine.erase(0,pos+1);  
+    }
+    temp.push_back(newLine);
+    return temp.size();
+}
+
+std::string info_container::TrimRight(const std::string & str) {
+    std::string tmp = str;
+    return tmp.erase(tmp.find_last_not_of(" ") + 1);
+}
+
+std::string info_container::TrimLeft(const std::string & str) {
+    std::string tmp = str;
+    return tmp.erase(0, tmp.find_first_not_of(" "));
+}
+
+std::string info_container::Trim(const std::string & str) {
+    std::string tmp = str;
+    return TrimLeft(TrimRight(str));
 }
--- project/src/info_constructor.h
+++ project/src/info_constructor.h
@@ -2,21 +2,60 @@
 #include <string>
 #include <vector>
 #include "math.h"
+
+#include <utility>
+#include <iostream>
+#include <map>
+#include <set>
+#include <cassert>
+#include <fstream>
+#include <utility>
+
 #define pi 3.1415926535897932384626433832795
 #define EARTH_RAD 6378.137 //unit: km
 //calculate distance in unit of km
-;
 
 class info_container
 {
 public:
-	info_container(std::string airports, std::string routes);
+	info_container(std::string & airports, std::string & routes);
+	void getAirportName();
+	void getAirportCode();
+	void getRoute();
+	void getStruct();
+
+	std::vector<std::vector<std::string>> airport_v;
+	std::vector<std::vector<std::string>> route_v;
+	std::vector<std::string> AirportName;
+	std::vector<std::string> AirportCode;
+	std::vector<std::pair<std::string, std::string>> allRoute;
+
 	void clean();
+	void cleanAirport();
+	void cleanRoute();
+
 	void read();
+	std::string readfile(const std::string & filename);
+	std::vector<std::vector<std::string>> transferFile(const std::string & filename);
+
+	int info_container::divide(const std::string &line, char ch, std::vector<std::string> &temp);
+	std::string TrimRight(const std::string & str);
+	std::string TrimLeft(const std::string & str);
+	std::string Trim(const std::string & str);
+
 	std::vector<std::string> generate_vertices();
 	std::vector<std::pair<std::string,std::string>> generate_edges();
 	std::vector<double> calculate_dist();
 	double cal_dist(double longitude1, double latitude1, double longitude2, double latitude2);
-private:
 
+	struct airports {
+		std::string name_;
+		std::string code_;
+		std::string latitude_;
+		std::string longtitude_;
+	};
+
+private:
+    std::string airports_;
+	std::string routes_;
 };
