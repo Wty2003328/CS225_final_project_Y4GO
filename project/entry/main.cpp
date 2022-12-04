b2aad578762585989763b9c68b87053aac2f6d40
Wty2003328 <116847915+Wty2003328@users.noreply.github.com>
2022-12-04 10:59:39 -0600

    minor update on main

--- project/entry/main.cpp
+++ project/entry/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-#include <vector>
 #include "info_constructor.h"
 #include "graph_constructor.h"
 using namespace std;
-
 int main()
 {
     info_container information("../data/airports.txt", "../data/routes.txt","../data/airlines.txt");
@@ -16,19 +14,41 @@ int main()
     string start,end;
     cout << "Your place of departure:" << endl;
     getline(cin,start);
+    bool find=false;
+    for(auto it=airports.begin();it!=airports.end();++it)
+    {
+        if(*it==start)
+        find=true;
+    }
+    if(find==false)
+    {
+        cout << "Invalid Input!" << endl;
+        return 0;
+    }
     cout << "Your destination:" << endl;
     getline(cin,end);
+    find=false;
+    for(auto it=airports.begin();it!=airports.end();++it)
+    {
+        if(*it==end)
+        find=true;
+    }
+    if(find==false)
+    {
+        cout << "Invalid Input!" << endl;
+        return 0;
+    }
     vector<Vertex> min_transfer = calculator.solvebyBFS(start, end);
     vector<Vertex> min_dist = calculator.solvebyDij(start, end);
-    cout << "minimum transfer route:" << endl;
+    cout << "\n"<<"minimum transfer route:" << endl;
     for (auto it = min_transfer.begin(); it != min_transfer.end(); ++it)
     {
-        cout << *it << " ";
+        cout << *it <<endl;
     }
-    cout << "minimum distance route:" << endl;
+    cout <<"\n"<<"minimum distance route:" << endl;
     for (auto it = min_dist.begin(); it != min_dist.end(); ++it)
     {
-        cout << *it << " ";
+        cout << *it <<endl;
     }
     return 0;
 }
