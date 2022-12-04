871dde5fe632c53287e6b4acafa838412d157efa
yilinzh4399 <yilin19@illinois.edu>
2022-12-03 19:33:29 -0600

    add 2 testcases

--- project/tests/testcases_data.cpp
+++ project/tests/testcases_data.cpp
@@ -29,3 +29,55 @@ TEST_CASE("try read correct airport and routes", "[weight=0][part=1]") {
     REQUIRE(routes[3].first="Goroka Airport");
     REQUIRE(routes[3].second="Port Moresby Jacksons International Airport");
 }
+
+
+TEST_CASE("try read correct airport and routes, some routes need to be delete", "[weight=0][part=1]") {
+
+    info_container information("../tests/information_test_airport3.txt", "../tests/information_test_routes3.txt");
+    information.read();
+    information.clean();
+    std::vector<Vertex> airports = information.generate_vertices();
+    std::vector<std::pair<Vertex, Vertex>> routes = information.generate_edges();
+    REQUIRE(airports.size()=8);
+    REQUIRE(routes.size()=8);
+    REQUIRE(airports[0]="Akureyri Airport");
+    REQUIRE(airports[1]="Egilsstaðir Airport");
+    REQUIRE(airports[2]="Hornafjörður Airport");
+    REQUIRE(airports[3]="Húsavík Airport");
+    REQUIRE(airports[4]="Ísafjörður Airport");
+    REQUIRE(airports[5]="Keflavik Airport");
+    REQUIRE(airports[6]="Patreksfjörður Airport");
+    REQUIRE(airports[7]="Reykjavik Airport");
+
+    REQUIRE(routes[0].first=="Akureyri Airport");
+    REQUIRE(routes[0].second="Egilsstaðir Airport");
+    REQUIRE(routes[1].first="Keflavik International Airport");
+    REQUIRE(routes[1].second="Patreksfjörður Airport");
+    REQUIRE(routes[2].first="Hornafjörður Airport");
+    REQUIRE(routes[2].second="Ísafjörður Airport");
+    REQUIRE(routes[3].first="Keflavik International Airport");
+    REQUIRE(routes[3].second="Hornafjörður Airport");
+}
+
+
+TEST_CASE("try read correct airport and routes, some airports need to be delete", "[weight=0][part=1]") {
+
+    info_container information("../tests/information_test_airport2.txt", "../tests/information_test_routes2.txt");
+    information.read();
+    information.clean();
+    std::vector<Vertex> airports = information.generate_vertices();
+    std::vector<std::pair<Vertex, Vertex>> routes = information.generate_edges();
+    REQUIRE(airports.size()=7);
+    REQUIRE(routes.size()=8);
+    REQUIRE(airports[0]="Akureyri Airport");
+    REQUIRE(airports[1]="Egilsstaðir Airport");
+    REQUIRE(airports[2]="Hornafjörður Airport");
+    REQUIRE(airports[3]="Ísafjörður Airport");
+    REQUIRE(airports[4]="Keflavik Airport");
+    REQUIRE(airports[5]="Patreksfjörður Airport");
+
+    REQUIRE(routes[0].first=="Akureyri Airport");
+    REQUIRE(routes[0].second="Egilsstaðir Airport");
+    REQUIRE(routes[1].first="Keflavik International Airport");
+    REQUIRE(routes[1].second="Hornafjörður Airport");
+}
